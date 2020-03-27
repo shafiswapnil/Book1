d74f61e4cad65eb7df224dff0d3b4701aa22bccf
ShafiAhmed <shafiahmedswapnil6494@gmail.com>
2020-03-27 17:45:19 +0600

    problem found in sentenceCount.js

--- Exercise1/sentenceCount.js
+++ Exercise1/sentenceCount.js
@@ -6,11 +6,16 @@ function sentenceCount(paragraph) {
 			count++;
 		}
 	}
-
 	return count;
 }
 
-var paragraph =
-	"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to";
+// var paragraph =
+// 	"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to";
 
-console.log(sentenceCount(paragraph));
+var output = sentenceCount("I'm so happy that i forgot my name!!!!");
+console.log(output);
+
+/* Problem
+ * when I add more than one (!) or (?) or (.) in one sentence,
+ * it assumes that theres more than one sentence
+ */
