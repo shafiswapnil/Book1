450b24fa19015ccb778e734bbd82c1ffdea2968d
ShafiAhmed <shafiahmedswapnil6494@gmail.com>
2020-03-27 17:49:49 +0600

    vowel detection success

---
+++ Exercise1/vowel.js
@@ -0,0 +1,19 @@
+// identify vowel words
+function vowel(sentence) {
+	var count = 0;
+	for (var i = 0; i < sentence.length; i++) {
+		if (
+			sentence[i] == "a" ||
+			sentence[i] == "e" ||
+			sentence[i] == "i" ||
+			sentence[i] == "o" ||
+			sentence[i] == "u"
+		) {
+			count++;
+		}
+	}
+	return count;
+}
+
+var output = vowel("My name is Swapnil. I have some work to do.");
+console.log(output);
