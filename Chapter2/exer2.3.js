d8417586116aa84db50c9852d0c10c70635f3277
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-04-25 12:42:40 +0600

    problem for exer2.3

---
+++ Chapter2/exer2.3.js
@@ -0,0 +1,5 @@
+/* Problem:
+ * Assume that, you have an array called Names. There are more than 5 elements in it. Now,
+ * you have to find the repeated ones but with a condition, which is - Keep the elements 
+ * who are odd times repeated and ignore the even ones.
+*/
