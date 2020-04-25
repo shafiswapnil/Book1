167ffcbae6eb1db6116db04fbb2f41d356a21575
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-04-25 12:48:04 +0600

    solution for exer2.3

--- Chapter2/exer2.3.js
+++ Chapter2/exer2.3.js
@@ -2,4 +2,34 @@
  * Assume that, you have an array called Names. There are more than 5 elements in it. Now,
  * you have to find the repeated ones but with a condition, which is - Keep the elements 
  * who are odd times repeated and ignore the even ones.
+ * 
+ * SOLUTION:
 */
+
+function getOddTimeRepeated(names) {
+	var uniqueNames = [];
+	var output = [];
+	var nameCount = {};
+
+	for ( var i = 0; i < names.length; i++ ) {
+		var name = names[i];
+		if ( uniqueNames.indexOf(name) == -1 ) {
+			uniqueNames.push(name);
+			nameCount[name] = 1;
+		} else {
+			nameCount[name] = nameCount[name] + 1;
+		}
+	}
+
+	for ( var i = 0; i < uniqueNames.length; i++ ) {
+		var name = names[i];
+		if ( nameCount[name]%2 != 0 ) {
+			output.push(name);
+		}
+	}
+
+	return output;
+}
+
+var names = ["pitu", "ritu", "jitu", "situ", "mitu", "nitu", "bitu", "jitu", "jitu", "bitu", "situ", "mitu", "gitu", "litu"];
+console.log(getOddTimeRepeated(names));
