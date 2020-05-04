39d9df6a6d61fdedeb7e9aa5ec6345aeb96fcf60
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-05-04 10:44:53 +0600

    some QnA for fibonacci in recursive way

--- Chapter6/fibonacci_recursive.js
+++ Chapter6/fibonacci_recursive.js
@@ -5,3 +5,11 @@ function fibonacci(n) {
 	return fibonacci(n - 1) + fibonacci(n - 2);
 }
 console.log(fibonacci(10));
+
+/*
+* Ques. Can you determine this fibonacci series in recursive way?
+* Ans. No. (Remember, you have to think before you answer.)
+*
+* Ques. What's the time complexity of fibonacci series in recursive way?
+* Ans. O(2^n) [for more info: www.habluderadda.com/gosti/fiboTime.html]
+*/
