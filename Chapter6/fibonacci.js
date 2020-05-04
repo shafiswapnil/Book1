5a7537e2e98eb1a0c3ed25446807ad68f514fb44
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-05-04 10:15:31 +0600

    fibonacci series in iterative way

--- Chapter6/fibonacci.js
+++ Chapter6/fibonacci.js
@@ -13,5 +13,14 @@
  * [i-1] and [i-2]
  * 2. As you know from the earlier that there is no array in the world, which has -1 and -2 positions.
  * An array starts from 0 not -1 or -2.
- */
+*/
 
+// As you will be running is function, you'll know it's an Iterative function.
+function fibonacci(n) {
+	var fibo = [0, 1];
+	for (var i = 2; i <= n; i++) {
+		fibo[i] = fibo[i - 1] + fibo[i - 2];
+	}
+	return fibo;
+}
+console.log(fibonacci(10));
