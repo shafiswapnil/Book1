0a8129975edfa611c9a34a0ebd8eb2f3d559d70e
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-05-05 20:54:10 +0600

    solution for exercise 3.3 (replace text in its place)

---
+++ Chapter3/exer3.3.js
@@ -0,0 +1,25 @@
+function reverseInPlace(text) {
+	var words = text.split(" ");
+	var reversed = [];
+
+	for (var i = 0; i < words.length; i++) {
+		var word = words[i];
+		var wordReversed = reverse(word);
+		reversed.push(wordReversed);
+	}
+
+	var reverseText = reversed.join(" ");
+
+	return reverseText;
+}
+
+function reverse(word) {
+	var reverseWord = "";
+	for (var i = word.length - 1; i >= 0; i--) {
+		var letter = word[i];
+		reverseWord = reverseWord + letter;
+	}
+	return reverseWord;
+}
+
+console.log(reverseInPlace("I am good boy"));
