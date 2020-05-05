b8d81ea4642277140290d94732827b7acd81c57b
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-05-05 20:48:12 +0600

    alternative solution for exercise 3.2 (sentence reversor function)

--- Chapter3/exer3.2.js
+++ Chapter3/exer3.2.js
@@ -1,13 +1,21 @@
+// function reverseSentence(text) {
+// 	var reverseText = "";
+// 	var words = text.split("");
+
+// 	for (var i = words.length - 1; i >= 0; i--) {
+// 		var word = words[i];
+// 		reverseText = reverseText + " " + word;
+// 	}
+
+// 	return reverseText;
+// }
+
+// Alternative Solution
 function reverseSentence(text) {
-	var reverseText = "";
-	var words = text.split("");
-
-	for (var i = words.length - 1; i >= 0; i--) {
-		var word = words[i];
-		reverseText = reverseText + " " + word;
-	}
-
-	return reverseText;
+	var words = text.split(" ");
+	var reversed = words.reverse();
+	var reversedText = reversed.join(" ");
+	return reversedText;
 }
 
-console.log(reverseSentence("I am a good boy."));
+console.log(reverseSentence("I am a good boy"));
